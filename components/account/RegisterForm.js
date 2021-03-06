57d994c803f028b6ac3ce4ab522e1e7d6976e580
jurgen <jurgen1010@hotmail.com>
2021-03-06 15:16:57 -0500

    Mostrar o ocultar la contraseña

--- components/account/RegisterForm.js
+++ components/account/RegisterForm.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Button, Input } from 'react-native-elements'
+import { Button, Icon, Input } from 'react-native-elements'
 
 export default function RegisterForm() {
+    const [showPassword, setShowPassword] = useState(false)     //State para controlar si se muestra o no la contraseña al hacer presion sobre el icono eye-outline
+
     return (
         <View style={styles.form}>
             <Input
@@ -13,13 +15,29 @@ export default function RegisterForm() {
                 containerStyle={styles.input}
                 placeholder="Ingresa tu contraseña..."
                 password ={true}
-                secureTextEntry={true}
+                secureTextEntry={!showPassword}
+                rightIcon={
+                    <Icon
+                        type="material-community"
+                        name={showPassword ? "eye-off-outline" : "eye-outline"} // Dependiendo del valor statePassword vamos alterar los inconos en pantalla
+                        iconStyle={styles.icon}
+                        onPress= {() => setShowPassword(!showPassword)}         //De esta manera al presionar alternaremos el estado del campo para mostror o no la contraeña al valor contrario de visualizacion
+                    />
+                }
             />
             <Input
                 containerStyle={styles.input}
                 placeholder="Confirma tu contraseña..."
                 password ={true}
-                secureTextEntry={true}
+                secureTextEntry={!showPassword}
+                rightIcon={
+                    <Icon
+                        type="material-community"
+                        name={showPassword ? "eye-off-outline" : "eye-outline"} // Dependiendo del valor statePassword vamos alterar los inconos en pantalla
+                        iconStyle={styles.icon}
+                        onPress= {() => setShowPassword(!showPassword)}         //De esta manera al presionar alternaremos el estado del campo para mostror o no la contraeña al valor contrario de visualizacion
+                    />
+                }    
             />
             <Button
                 title="Registrar Nuevo Usuario"
@@ -45,6 +63,10 @@ const styles = StyleSheet.create({
     },
     btn:{
         backgroundColor: "#713853"
+    },
+    icon: {
+        color: "#c1c1c1"     //Color opacidad
+
     }
 
 })
