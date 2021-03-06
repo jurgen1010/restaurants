640f0383f920e63c89fb30b85749b01b7df968f7
jurgen <jurgen1010@hotmail.com>
2021-03-06 12:58:34 -0500

    completando pantalla de usuario invitado

--- screens/account/UserGuest.js
+++ screens/account/UserGuest.js
@@ -1,12 +1,53 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text,  ScrollView, Image } from 'react-native'
+import { Button } from 'react-native-elements'
 
 export default function UserGuest() {
     return (
-        <View>
-            <Text>UserGuest...</Text>
-        </View>
+        <ScrollView
+            centerContent
+            style={styles.viewBoddy}
+        >
+            <Image
+                source={require("../../assets/Restaurant_logo.png")}
+                resizeMode="contain"
+                style={styles.image}
+            />
+            <Text style={styles.title}>Consulta tu perfil en Restaurants</Text>
+            <Text style={styles.descripcion}>
+                ¿Como describirías tu mejor restaurante? Busca y visualiza los mejores restaurantes de una forma sencilla, vota cuál te ha gustado más y comenta cómo ha sido tu experiencia.
+            </Text>
+            <Button
+                buttonStyle={styles.button}
+                title = "Ver tu perfil"
+                onPress={()=> console.log("Click!!!")}
+            ></Button>
+        </ScrollView>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    viewBoddy: {
+        marginHorizontal: 30
+    },
+    image: {
+        height: 250,
+        width: "100%",
+        marginBottom: 10,
+        textAlign: "center"
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 19,
+        marginVertical: 10,
+        textAlign: "center"
+    },
+    descripcion: {
+        textAlign: "justify",
+        marginBottom: 20,
+        color: "#c42434"
+    },
+    button:{
+        backgroundColor: "#713853"
+    }
+})
