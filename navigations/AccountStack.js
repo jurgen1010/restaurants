2842ca9cb927b7ed08173ada6d23c90aac1e7e48
jurgen <jurgen1010@hotmail.com>
2021-03-06 14:34:04 -0500

    Creando formulario registrar usuario

--- navigations/AccountStack.js
+++ navigations/AccountStack.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import Account from '../screens/account/Account'
 import Login from '../screens/account/Login'
+import Register from '../screens/account/Register'
+
 
 
 const Stack = createStackNavigator()
@@ -18,6 +20,11 @@ export default function AccountStack() {
                 name="login"
                 component={Login}
                 options = {{ title : "Iniciar Sesión" }}
+            />
+            <Stack.Screen   //Adicionamos un stack screen para poder ir a la pantalla de Register
+                name="register"
+                component={Register}
+                options = {{ title : "Registrar Usuario" }}
             />
         </Stack.Navigator>
     )
