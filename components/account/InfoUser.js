45022f224fce98cf93d41bcd5365177f086af99f
jurgen <jurgen1010@hotmail.com>
2021-03-09 12:49:04 -0500

    Iniciando con la pantalla del usuario

---
+++ components/account/InfoUser.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function InfoUser({ user }) {    //Hacemos detructuring del parametro enviado por props desde UserLogged
+    return (
+        <View>
+            <Text>InfoUser...</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
