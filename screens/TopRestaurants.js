73fe3f29fc73555aa781a9890c8f954c080ca803
jurgen <jurgen1010@hotmail.com>
2021-05-02 10:10:12 -0500

    Obteniendo la lista de los mejores restaurantes

--- screens/TopRestaurants.js
+++ screens/TopRestaurants.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState,  useCallback } from 'react'
+import { useFocusEffect } from '@react-navigation/native'
 import { StyleSheet, Text, View } from 'react-native'
 
+import { getTopRestaurants } from '../utils/actions'
+import Loading from '../components/Loading'
+
 export default function TopRestaurants() {
+    const [restaurants, setRestaurants] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    console.log(restaurants)
+
+    useFocusEffect(
+        useCallback(() => {
+            async function getData(){
+                setLoading(true)
+                const response = await getTopRestaurants(10)
+                if (response.statusResponse) {
+                    setRestaurants(response.restaurants)
+                }
+                setLoading(false)
+            }
+            getData()
+        }, [])
+    )
     return (
         <View>
             <Text>TopRestaurants</Text>
+            <Loading isVisible={loading} text={"Por favor espere..."}/>
         </View>
     )
 }
